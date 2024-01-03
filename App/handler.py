a0bb0169264c7ce44f3e74f2f3ff2965cd649287
jcamacho <juan.camacho@proper.ai>
2024-01-03 16:39:02 -0600

    Offset

--- App/handler.py
+++ App/handler.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from read import analizar_documento
+from mocks import respuestas_mocks
 import os
 
 
@@ -11,8 +12,16 @@ class Devocional(Resource):
     def get(self):
         resp = analizar_documento('template2.docx')
         return jsonify(resp)
+    
+class Mocks(Resource):
+    def get(self):
+        page = request.args.get('page', default = 1, type = int)
+        per_page = request.args.get('per_page', default = 10, type = int)
+        resp = respuestas_mocks(page, per_page)
+        return jsonify(resp)
 
-api.add_resource(Devocional, '/')
+api.add_resource(Devocional, '/devocional')
+api.add_resource(Mocks, '/mocks')
 
 if __name__ == '__main__':
     app.run(debug=True)
