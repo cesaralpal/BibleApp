17d324837e98a7ed815cd8b215d9582d3ff525b1
Julio Cesar Alcibar Palacios <alcibar@MacBook-Pro-de-Julio.local>
2024-01-12 13:29:25 -0600

    delete app

--- app/usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS usuarios (
-    id UUID DEFAULT uuid_generate_v4(),
-    nombre TEXT NOT NULL,
-    correo TEXT NOT NULL,
-    fcm_token VARCHAR(255)
-);
